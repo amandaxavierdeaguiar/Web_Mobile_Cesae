9aa25e13f36dc9ace687d8b128302982bccdf502
amandaxavierdeaguiar <amandaxavierdeaguiar@gmail.com>
2024-07-16 00:14:28 +0100

    atividade_aula

---
+++ Algoritmia/LancamentosDados/AppDados.java
@@ -0,0 +1,39 @@
+// Importar o package necessário
+import java.util.Random;
+
+// Definição da classe
+class AppDados
+{
+    //Construtor
+    AppDados()
+    {
+        System.out.println("O programa vai lançar 5 dados:");
+        
+        /*for(int i = 0; i <= 4; i++){
+            System.out.println("O número escolhido foi: "+lancarDado());
+        }*/
+        
+        System.out.println("O número escolhido foi: "+ lancarDado());
+        System.out.println("O número escolhido foi: "+ lancarDado());
+        System.out.println("O número escolhido foi: "+ lancarDado());
+        System.out.println("O número escolhido foi: "+ lancarDado());
+        System.out.println("O número escolhido foi: "+ lancarDado());
+        
+        }
+    
+    int lancarDado(){
+        Random numeroAleatorio = new Random();
+        
+        int dado = numeroAleatorio.nextInt(6) + 1;
+        
+        return dado;
+    }
+    
+    /**String maneiraAndre(){
+     * Random numeroAleatorio = new Random();
+     * int dado = numeroAleatorio.nextInt(6) + 1;
+     * 
+     * return "O programa vai lançar um dado: " + dado;
+     * }
+     */
+}
---
+++ Web_Client/Exercicio_aula/README.md
@@ -0,0 +1,17 @@
+
+# 🧑‍💻 Exercício 
+## Criar uma página HTML & CSS
+
+Atividade de implementação do HTML e CSS da aula de Desenvolvimento Web – Client-Side com a professora Lais Reis.
+
+O projeto ainda não se encontra responsivo, vou adaptando e acrescentando conteúdos aos poucos.
+
+
+<img width="800" src="index.png">
+<img width="800" src="portfolio.png">
+<img width="800" src="formulario_contato.png">
+
+
+## Autores
+
+- [@amandaxavierdeaguiar](https://github.com/amandaxavierdeaguiar)
